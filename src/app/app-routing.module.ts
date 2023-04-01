e57565b75cf3b8a09a417d82519423d9227a00c0
Marcone Melo Mendonça <marconemendonca@ymail.com>
2023-03-31 21:56:17 -0300

    feat: implement the contato component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BalancesComponent } from './components/balances/balances.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'extratos',
     component: BalancesComponent,
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent,
   },
 ];
 
